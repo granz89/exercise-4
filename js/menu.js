47c81bfadc7301ef6a9c8e67239a37cada840b17
granz89 <a.granieri@hotmail.com>
2015-02-09 10:44:52 -0500

    feat(js): add toggle

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,10 @@
 // Needs latest version of jQuery to run
+// This refers to .dropdown-btn
+// If doesn't work play with the class
 
 $('.dropdown-btn').click(function(){
   $(this).next().slideToggle(150);
+  $('html, body').animate({
+      scrollTop: $(this).offset().top + $('window').height()
+    }, 2000);
 });
